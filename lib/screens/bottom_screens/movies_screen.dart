9318afc7460d4a063e72575047287c848bc45f94
abhpandey <abhishekpaney@gmail.com>
2025-12-16 10:52:11 +0545

    removed appbar in movies screen

--- lib/screens/bottom_screens/movies_screen.dart
+++ lib/screens/bottom_screens/movies_screen.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
 
-class MoviesScreen extends StatefulWidget {
+class MoviesScreen extends StatelessWidget {
   const MoviesScreen({super.key});
 
   @override
-  State<MoviesScreen> createState() => _MoviesScreenState();
-}
-
-class _MoviesScreenState extends State<MoviesScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 207, 38, 38),
-      appBar: AppBar(
-        
-        backgroundColor: const Color.fromARGB(255, 235, 89, 89),
-      ),
-      body: const Center(
-        child: Text(
-          "Movies Screen",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
+    return const Center(
+      child: Text(
+        "Movies Screen",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
