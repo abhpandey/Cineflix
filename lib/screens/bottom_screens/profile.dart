47a4b530f82f339c7d28c1de4573ea30990df838
abhpandey <abhishekpaney@gmail.com>
2025-12-16 10:32:48 +0545

    background color added in profile screen

--- lib/screens/bottom_screens/profile.dart
+++ lib/screens/bottom_screens/profile.dart
@@ -10,6 +10,23 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 207, 38, 38),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 235, 89, 89),
+        title: const Text("Profile"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          "Profile Screen",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
