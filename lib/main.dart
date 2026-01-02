954714b55156043473601edf04aa302f095a3bb1
abhpandey <abhishekpaney@gmail.com>
2026-01-02 12:29:03 +0545

    added hiveservice boxes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
-import 'package:ceniflix/app.dart';
+import 'package:ceniflix/app/app.dart';
+import 'package:ceniflix/core/services/hive/hive_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final hiveService = HiveService();
+  await hiveService.init();
+  await hiveService.openBoxes(); 
+
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
