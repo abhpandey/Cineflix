d628c0471ae6e7faf1d9f20034b2dd023d797fa4
abhpandey <abhishekpaney@gmail.com>
2026-01-16 11:24:25 +0545

    Update main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,39 @@
 import 'package:ceniflix/app/app.dart';
 import 'package:ceniflix/core/services/hive/hive_service.dart';
+import 'package:ceniflix/core/services/storage/user_session_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final hiveService = HiveService();
-  await hiveService.init();
-  await hiveService.openBoxes(); 
+  try {
+    final hiveService = HiveService();
+    await hiveService.init();
+    await hiveService.openBoxes();
 
-  runApp(
-    const ProviderScope(
-      child: App(),
-    ),
-  );
+    final sharedPreferences = await SharedPreferences.getInstance();
+
+    runApp(
+      ProviderScope(
+        overrides: [
+          sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+        ],
+        child: const App(),
+      ),
+    );
+  } catch (e) {
+    debugPrint('Error initializing app: $e');
+
+    runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Error initializing app: $e'),
+          ),
+        ),
+      ),
+    );
+  }
 }
