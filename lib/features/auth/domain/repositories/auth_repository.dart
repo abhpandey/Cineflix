5254141e59b2c04edbf854dc1fc38da35ad87460
abhpandey <abhishekpaney@gmail.com>
2026-01-16 11:19:51 +0545

    Update auth domain repository interface

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -4,7 +4,7 @@ import 'package:ceniflix/features/auth/domain/entities/auth_entity.dart';
 import 'package:dartz/dartz.dart';
 
 
-abstract interface class IauthRepository {
+abstract interface class IAuthRepository {
   Future<Either<Failure, bool>> register(AuthEntity entity);
   Future<Either<Failure, AuthEntity>> login(String email, String password);
   Future<Either<Failure, AuthEntity>> getCurrentUser();
