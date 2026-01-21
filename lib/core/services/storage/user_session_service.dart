3de6ddb003043654a174222bb54fc8d59ec37bc4
abhpandey <abhishekpaney54@gmail.com>
2026-01-21 20:18:12 +0545

    comment removed

--- lib/core/services/storage/user_session_service.dart
+++ lib/core/services/storage/user_session_service.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// SharedPreferences instance provider
 final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
   throw UnimplementedError('SharedPreferences must be overridden in main.dart');
 });
